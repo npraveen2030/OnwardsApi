38ffbf80adbe936fff83b33d4160abad5131c0b8
prasaditham <prasad.itham@gmail.com>
2025-07-22 13:40:31 +0530

    added

--- DB/011_Onwards_All.sql
+++ DB/011_Onwards_All.sql
@@ -31,3 +31,53 @@ GO
 INSERT [Onwards].[UserShiftLog] ([LogId], [UserId], [ShiftId], [LoginTime], [Date], [LogOutTime], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate], [IsActive]) VALUES (6, 1, 1, CAST(N'17:10:14.6333333' AS Time), CAST(N'2025-07-21' AS Date), CAST(N'22:44:15.6566667' AS Time), 1, CAST(N'2025-07-21T17:10:14.633' AS DateTime), NULL, NULL, 1)
 GO
 SET IDENTITY_INSERT [Onwards].[UserShiftLog] OFF
+
+
+ALTER PROCEDURE [Onwards].[GetUserShiftDetails]
+    @UserId INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @Today DATE = CAST(GETDATE() AS DATE);
+
+    SELECT 
+        U.FullName,
+        S.ShiftName,
+
+        -- Date formatted as 07-Jul-2025
+        FORMAT(@Today, 'dd-MMM-yyyy') AS TodayDate,
+
+        -- Shift start time formatted as 9:00 am
+       -- LOWER(CONVERT(VARCHAR(15), S.StartTime, 100)) AS ShiftStartTime,
+        
+        LOWER(STUFF(RIGHT(CONVERT(VARCHAR(20), S.StartTime, 100), 7), 5, 0, ' ')) AS ShiftStartTime,
+
+        -- Login/Logout times formatted as 12:59 am/pm
+        --LOWER(RIGHT(CONVERT(VARCHAR(20), USL.LoginTime, 100), 7)) AS LoginTime,
+        --LOWER(RIGHT(CONVERT(VARCHAR(20), USL.LogOutTime, 100), 7)) AS LoginTime,
+        LOWER(STUFF(RIGHT(CONVERT(VARCHAR(20), USL.LoginTime, 100), 7), 6, 0, ' ')) AS LoginTime,
+        LOWER(STUFF(RIGHT(CONVERT(VARCHAR(20), USL.LogOutTime, 100), 7), 6, 0, ' ')) AS LogoutTime,
+
+
+        -- Total logged-in duration formatted as 03:57 Hrs
+        CASE 
+            WHEN USL.LoginTime IS NOT NULL AND USL.LogOutTime IS NOT NULL
+            THEN 
+                RIGHT('00' + CAST(DATEDIFF(MINUTE, USL.LoginTime, USL.LogOutTime) / 60 AS VARCHAR(2)), 2)
+                + ':' +
+                RIGHT('00' + CAST(DATEDIFF(MINUTE, USL.LoginTime, USL.LogOutTime) % 60 AS VARCHAR(2)), 2)
+                + ' Hrs'
+            ELSE NULL
+        END AS TotalLoggedInHours
+    FROM [Onwards].[Users] AS U
+    INNER JOIN [Onwards].[UserShiftAssignment] AS USA 
+        ON U.Id = USA.UserId
+    INNER JOIN [Onwards].[Shift] AS S 
+        ON USA.ShiftId = S.ShiftId
+    LEFT JOIN [Onwards].[UserShiftLog] AS USL 
+        ON U.Id = USL.UserId
+       AND USL.Date = @Today  -- Replace LogDate with actual date column if exists
+    WHERE U.Id = @UserId;
+END;
+GO
