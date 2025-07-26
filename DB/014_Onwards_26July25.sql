762d4f075bc1b821b49a8c0957cd31de85c62f20
npraveen2030 <npraveen2030@gmail.com>
2025-07-26 17:21:18 +0530

    Added

--- DB/014_Onwards_26July25.sql
+++ DB/014_Onwards_26July25.sql
@@ -1,3 +1,22 @@
+
+
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+Create PROCEDURE [Onwards].[sp_ValidateUserLogin]
+    @EmployeeCode NVARCHAR(50),
+    @Password NVARCHAR(100)
+AS
+BEGIN
+    SELECT U.EmployeeCode,U.FullName, U.Email, R.RoleName FROM Onwards.Users as U
+	INNER JOIN Onwards.BasicDetails as BD ON U.id= BD.Userid
+	INNER JOIN Onwards.Roles as R on U.RoleId = R.Id 
+	WHERE U.Isactive = 1 AND BD.Isactive = 1 and R.isActive =1
+    AND EmployeeCode = @EmployeeCode AND Password = @Password;
+END;
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Table: Reimbursement
 
 CREATE TABLE Onwards.Reimbursement (
